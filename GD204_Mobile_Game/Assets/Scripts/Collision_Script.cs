45d874a1357a28317d7bf9d658705b06eafd2c5d
SidneyAdams <sidadams04@outlook.com>
2024-04-15 10:16:05 +0100

    Test Build Commit
    I got one of the sound effects to work and created a test build to test certain functionality.

--- GD204_Mobile_Game/Assets/Scripts/Collision_Script.cs
+++ GD204_Mobile_Game/Assets/Scripts/Collision_Script.cs
@@ -6,7 +6,10 @@ using UnityEngine.SceneManagement;
 public class Collision_Script : MonoBehaviour
 {
     bool gameOver;
-    AudioSource audioData;
+    public AudioClip boost;
+    public AudioClip spike;
+    public AudioClip mine;
+    public AudioClip civCar;
 
     // Start is called before the first frame update
     void Start()
@@ -25,10 +28,26 @@ public class Collision_Script : MonoBehaviour
         {
             Destroy(other.gameObject);
             Debug.Log("PickUp");
+            if (other.gameObject.name =="Nitrous_Canister_Prefab" || other.gameObject.name == "Nitrous_Canister_Prefab(Clone)")
+            {
+                AudioSource.PlayClipAtPoint(boost, gameObject.transform.position);
+            }
         }
         if (other.gameObject.tag == "Obstacle")
         {
-            gameOver = true;
+            if (other.gameObject.name == "Spike_Prefab" || other.gameObject.name == "Spike_Prefab(Clone)")
+            {
+                AudioSource.PlayClipAtPoint(spike, gameObject.transform.position);
+            } 
+            if (other.gameObject.name == "Mine_Prefab" || other.gameObject.name == "Mine_Prefab(Clone)")
+            {
+                AudioSource.PlayClipAtPoint(mine, gameObject.transform.position);
+            } 
+            if (other.gameObject.name == "Civilian_Car_Prefab" || other.gameObject.name == "Civilian_Car_Prefab(Clone)")
+            {
+                AudioSource.PlayClipAtPoint(civCar, gameObject.transform.position);
+            }
+            gameOver = true;            
         }
 
         if (gameOver == true)
