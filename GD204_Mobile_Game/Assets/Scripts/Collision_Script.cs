ff675557a2a09a0bba513aea463a0b72d23c89af
SidneyAdams <sidadams04@outlook.com>
2024-04-19 15:53:13 +0100

    Touchscreen Commit
    I have added the swipe controls and some powerups to the game, as well as fixing some prefab and UI issues and adding Harry's Icon

--- GD204_Mobile_Game/Assets/Scripts/Collision_Script.cs
+++ GD204_Mobile_Game/Assets/Scripts/Collision_Script.cs
@@ -10,11 +10,21 @@ public class Collision_Script : MonoBehaviour
     public AudioClip spike;
     public AudioClip mine;
     public AudioClip civCar;
+   // public GameObject player;
+    public GameObject timeManager;
+    Timer timer;
+    bool colliderEnabled = true;
+    //float playerSpeed;
+    
+       
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+       
     }
     // Update is called once per frame
     void Update()
@@ -31,13 +41,29 @@ public class Collision_Script : MonoBehaviour
             if (other.gameObject.name =="Nitrous_Canister_Prefab" || other.gameObject.name == "Nitrous_Canister_Prefab(Clone)")
             {
                 AudioSource.PlayClipAtPoint(boost, gameObject.transform.position);
+               // playerSpeed = player.GetComponent<PlayerMovnemt>().moveSpeed;
+               // playerSpeed += 5;
+
             }
+            if (other.gameObject.name == "Bumper_Bar_Prefab" || other.gameObject.name == "Bumper_Bar_Prefab(Clone)")
+            {
+                colliderEnabled = false;                
+                             
+                
+            }
+           if (other.gameObject.name == "Vial_Prefab" || other.gameObject.name == "Vial_Prefab(Clone)")
+           {
+                timer = timeManager.GetComponent<Timer>();
+                timer.currentTime += 5;                              
+                  
+                 
+           }
         }
-        if (other.gameObject.tag == "Obstacle")
+        if (other.gameObject.tag == "Obstacle") 
         {
             if (other.gameObject.name == "Spike_Prefab" || other.gameObject.name == "Spike_Prefab(Clone)")
             {
-                AudioSource.PlayClipAtPoint(spike, gameObject.transform.position);
+                AudioSource.PlayClipAtPoint(spike, gameObject.transform.position);               
             } 
             if (other.gameObject.name == "Mine_Prefab" || other.gameObject.name == "Mine_Prefab(Clone)")
             {
@@ -47,7 +73,11 @@ public class Collision_Script : MonoBehaviour
             {
                 AudioSource.PlayClipAtPoint(civCar, gameObject.transform.position);
             }
-            gameOver = true;            
+            if(colliderEnabled == true)
+            {
+                gameOver = true;
+            }
+            colliderEnabled = true;
         }
 
         if (gameOver == true)
--- GD204_Mobile_Game/Assets/Scripts/PlayerMovnemt.cs
+++ GD204_Mobile_Game/Assets/Scripts/PlayerMovnemt.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class PlayerMovnemt : MonoBehaviour
 {
-    public float moveSpeed = 3;
-    public float leftRightSpeed = 4;
+    public float moveSpeed = 20;
+    public float leftRightSpeed = 12.5f;
 
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow) || (SwipeControls.swipeRight))
         {
             if (this.gameObject.transform.position.x > LevelBoundary.leftSide)
             {
                 transform.Translate(Vector3.left * Time.deltaTime * leftRightSpeed);
             }
         }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) || (SwipeControls.swipeRight))
         {
             if (this.gameObject.transform.position.x < LevelBoundary.rightSide)
             {
